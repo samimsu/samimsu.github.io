13d13cdf22831a08db25d997892daae6425b7d21
Sami <mohammadsamimsu@gmail.com>
2024-04-06 22:57:57 +0400

    Build initial home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,17 @@
 import RootLayout from "../layouts/RootLayout";
-import UnderConstruction from "../components/UnderConstruction";
 
 const Home = () => {
   return (
     <RootLayout>
-      <UnderConstruction />
+      <div className="relative max-w-4xl mx-auto pt-20 sm:pt-24 lg:pt-32">
+        <h1 className="text-slate-900 font-medium !leading-tight text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white z-50">
+          I'm a{" "}
+          <span className="relative sm:after:underline-svg sm:dark:after:underline-svg-dark">
+            <span className="relative z-10">Web Developer</span>
+          </span>{" "}
+          primarily based in the UAE
+        </h1>
+      </div>
     </RootLayout>
   );
 };
