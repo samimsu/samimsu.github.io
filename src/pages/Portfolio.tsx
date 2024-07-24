f738b643eb9953172a1226ad215c4b1bac8d7698
Sami <mohammadsamimsu@gmail.com>
2024-07-24 13:17:10 +0500

    Update Portfolio.tsx

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -10,15 +10,15 @@ import NewTabIcon from "../components/NewTabIcon";
 const Portfolio = () => {
   const { isLightMode } = useThemeContext();
 
-  const renderExperienceSection = () => {
-    return (
-      <div>
-        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-          <Heading2>Experience</Heading2>
-        </div>
-      </div>
-    );
-  };
+  // const renderExperienceSection = () => {
+  //   return (
+  //     <div>
+  //       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+  //         <Heading2>Experience</Heading2>
+  //       </div>
+  //     </div>
+  //   );
+  // };
 
   const renderProjectsSection = () => {
     return (
