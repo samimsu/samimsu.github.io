7672732707bf89db2134e9c5f92d0b03bee1c780
Sami <mohammadsamimsu@gmail.com>
2024-07-24 13:13:05 +0500

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <RootLayout>
       <div className="relative max-w-4xl mx-auto pt-20 sm:pt-24 lg:pt-32">
-        <h1 className="text-slate-900 font-medium !leading-tight text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white z-50">
+        <h1 className="text-slate-900 font-medium !leading-tight text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-slate-200 z-50">
           I'm a{" "}
           <span className="relative sm:after:underline-svg sm:dark:after:underline-svg-dark">
             <span className="relative z-10">Web Developer</span>
