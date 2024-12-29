5b5e1df79ce5d43854e36d6b8339fb6f0cc73593
Sami <mohammadsamimsu@gmail.com>
2024-12-29 17:39:57 +0500

    Add banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,17 +1,44 @@
+import { useState } from "react";
+
 type BannerProps = {
   close: () => void;
 };
 
 const Banner = ({ close }: BannerProps) => {
+  const [copiedEmail, setCopiedEmail] = useState(false);
+  const myEmail = "mohammadsamimsu@gmail.com";
+
   return (
     <div
       id="sticky-banner"
-      className="fixed bottom-0 start-0 z-50 flex justify-between w-full p-4 border-b border-gray-200 bg-gray-50 dark:bg-gray-700 dark:border-gray-600"
+      className="start-0 z-50 flex justify-between w-full p-2 border-b border-gray-200 bg-gray-50 dark:bg-gray-700 dark:border-gray-600"
     >
-      <div className="flex items-center mx-auto">
-        <p className="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
-          <span>Note: This site is still under development</span>
+      <div className="flex flex-col sm:flex-row sm:space-x-2 space-y-1 sm:space-y-0 items-center mx-auto">
+        <p className="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400 space-x-1">
+          <span>
+            I'm actively looking for work. If you have anything at all, consider
+            reaching out. No work is too small. Thanks a lot!{" "}
+          </span>
         </p>
+        <div className="flex space-x-2">
+          <a href={`mailto:${myEmail}`}>
+            <button className="whitespace-nowrap rounded bg-blue-500 text-white dark:text-slate-900 px-1.5 py-1 hover:bg-blue-600">
+              Email me
+            </button>
+          </a>
+          <button
+            className="whitespace-nowrap rounded border border-blue-500 text-blue-500 px-1.5 py-1 hover:border-blue-600"
+            onClick={async () => {
+              navigator.clipboard.writeText(myEmail);
+              setCopiedEmail(true);
+              setTimeout(() => {
+                setCopiedEmail(false);
+              }, 1500);
+            }}
+          >
+            {copiedEmail ? "Copied" : "Copy email"}
+          </button>
+        </div>
       </div>
       <div className="flex items-center">
         <button
