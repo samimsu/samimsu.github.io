7b0ead00d76cebd75c460a486603344aaeea8097
Sami <mohammadsamimsu@gmail.com>
2025-01-02 23:59:37 +0500

    Remove banner

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,19 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import Banner from "../components/Banner";
-import { useBannerContext } from "../hooks/useBannerContext";
 
 type LayoutProps = {
   children: React.ReactNode;
 };
 
 const RootLayout = ({ children }: LayoutProps) => {
-  const { showBanner, closeBanner } = useBannerContext();
-
   return (
     <div className="flex flex-col h-full overflow-clip">
-      {showBanner && <Banner close={closeBanner} />}
       <Header />
       <div className="flex-1 min-h-[500px]">{children}</div>
       <Footer />
